1c0a1408fe1ded7de2a7d0ef0f75f43111d0ba5b
templaterbd <deividohome@gmail.com>
2025-10-05 15:48:21 +0300

    java test

---
+++ test/java/com/example/demo/ApplicationTest.java
@@ -0,0 +1,9 @@
+package test.java.com.example.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTest {
+    @Test
+    void contextLoads() {}
+}
